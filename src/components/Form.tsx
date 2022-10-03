b68c01a37929e01af396ff0d38d0478d6e94b34b
TakTadano <ms6rx7861@gmail.com>
2022-10-03 23:09:21 +0900

    Form部分作成

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,16 +1,23 @@
 //Form.tsx
 
+import { analyze } from "eslint-scope";
 import { useState } from "react";
 
 
 const Form = () => {
     const [city, setCity] = useState<string>();
+    const getWeather = (e:any) => {
+        e.preventDefault();
+        fetch("https://api.weatherapi.com/v1/current.json?key=9ff2e9b8c07a482e8aa125849220310&q=London&aqi=no")
+        .then(res => console.log(res.json()))
+        .then(data => console.log(data))
+    }
     return (
         <form action="">
             <input type="text" name="city" placeholder="都市名" onChange={e => setCity(e.target.value)}/>
             {city}
             
-            <button type="submit">Get Weather</button>
+            <button type="submit" onClick={getWeather}>Get Weather</button>
         </form>
 
     );
@@ -18,3 +25,6 @@ const Form = () => {
 
 
 export default Form;
+
+
+//https://api.weatherapi.com/v1/current.json?key=9ff2e9b8c07a482e8aa125849220310&q=London&aqi=no
